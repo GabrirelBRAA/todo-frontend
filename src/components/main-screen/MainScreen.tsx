014c8f03a6453070db20a9691f0561a1eeb0fc20
GabrirelBRAA <jgbiel29@gmail.com>
2025-09-04 14:24:11 -0300

    Added list item to main app component now syncing with BE
    This is just a test, I plan on adding a loading icon on together with
    suspense so that slow downs in the query dont leave the UI empty.

--- src/components/main-screen/MainScreen.tsx
+++ src/components/main-screen/MainScreen.tsx
@@ -1,36 +1,53 @@
 import { Nav } from "../nav/Nav";
 import styles from "./style.module.css";
+import { itemService, type Item } from "../../services/itemService";
+import { useEffect, useState } from "react";
 // top nav
 // add button
 // dialog to create item
 // list of users items
 // paginator component
 
-function Item() {
+function Item({item}: {item: Item}) {
 	return (
 		<div className={styles.item}>
-			<div className={styles.itemheader}>header</div>
-			<div>content</div>
+			<div className={styles.itemheader}>{item.title}</div>
+			<div>{item.description}</div>
 		</div>
 	);
 }
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export function MainApp() {
+
+    const [items, setItems] = useState<Array<Item>>([])
+
+    async function getItems(){
+        const items = await itemService.list(0, 12)
+        await timeout(1000)
+        setItems(items)
+        console.log(items)
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    const itemsComponents = items.map((item, index) => <Item key={index} item={item}/>)
+
 	return (
 		<>
 			<Nav />
 			<div className={styles.container}>
 				<button className={styles.addbutton}>New</button>
 				<div className={styles.listcontainer}>
-					<Item />
-					<Item />
-					<Item />
-					<Item />
-					<Item />
-					<Item />
-					<Item />
+                    {itemsComponents}
 
 				</div>
+                <div>Pagination</div>
 			</div>
 		</>
 	);
