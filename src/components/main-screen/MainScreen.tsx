b48de92c0b342fb98bfe6707bd33f52a7bd0781b
GabrirelBRAA <jgbiel29@gmail.com>
2025-09-04 17:41:31 -0300

    Added modal logic to MainApp component
    Will probably make it more reusable later, but now I have a basic modal
    form to implement item creation with the BE.

--- src/components/main-screen/MainScreen.tsx
+++ src/components/main-screen/MainScreen.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 // list of users items
 // paginator component
 
-function Item({item}: {item: Item}) {
+function Item({ item }: { item: Item }) {
 	return (
 		<div className={styles.item}>
 			<div className={styles.itemheader}>{item.title}</div>
@@ -17,40 +17,69 @@ function Item({item}: {item: Item}) {
 	);
 }
 
+function Modal({open, closeModal} : {open: boolean, closeModal: () => void}) {
+	return (
+		<>
+			{open ? (<div className={styles.modaloverlay}></div>) : null}
+			<dialog open={open} className={styles.modal}>
+				<h2>Create new item</h2>
+				<button className={styles.closebutton} onClick={closeModal}>X</button>
+				<form>
+					<label>
+						<input type="text" placeholder="Title" />
+					</label>
+					<label>
+						<textarea />
+					</label>
+					<button type="submit">Save</button>
+				</form>
+			</dialog>
+		</>
+	);
+}
+
 //just a test function that helps with slowing down queries
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function MainApp() {
+	const [items, setItems] = useState<Array<Item>>([]);
+	const [totalCount, setTotalCount] = useState<number>(0);
+    const [modalOpen, setModalOpen] = useState(false)
 
-    const [items, setItems] = useState<Array<Item>>([])
-    const [totalCount, setTotalCount] = useState<number>(0)
-
-    async function getItems(){
-        const items = await itemService.list(0, 12)
-        await timeout(1000)
-        setItems(items.items)
-        setTotalCount(items.total_count)
-        console.log(items)
+    const closeModal = () => {
+        setModalOpen(false)
     }
 
-    useEffect(() => {
-        getItems()
-    }, [])
+    const openModal = () => {
+        setModalOpen(true)
+    }
 
-    const itemsComponents = items.map((item, index) => <Item key={index} item={item}/>)
+	async function getItems() {
+		const items = await itemService.list(0, 12);
+		await timeout(1000);
+		setItems(items.items);
+		setTotalCount(items.total_count);
+		console.log(items);
+	}
+
+	useEffect(() => {
+		getItems();
+	}, []);
+
+	const itemsComponents = items.map((item, index) => (
+		<Item key={index} item={item} />
+	));
 
 	return (
 		<>
 			<Nav />
 			<div className={styles.container}>
-				<button className={styles.addbutton}>New</button>
-				<div className={styles.listcontainer}>
-                    {itemsComponents}
-
-				</div>
-                <div>Pagination {totalCount}</div>
+			    <button className={styles.addbutton} onClick={openModal}>New</button>
+				<Modal open={modalOpen} closeModal={closeModal}/>
+				<div className={styles.listcontainer}>{itemsComponents}</div>
+				<div>Pagination {totalCount}</div>
 			</div>
 		</>
 	);
